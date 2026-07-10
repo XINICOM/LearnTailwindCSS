4d16c4cf38368acceba134bfcc54027a0bfc3a15
XINICOM <XINICOM@outlook.com>
2026-07-10 23:12:10 +0800

    theme button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
-    darkMode: "media",
+    darkMode: "class",
     theme: {
         extend: {
-        fontFamily: {
-            serif: ['宋体', 'SimSun', 'Songti SC', 'serif'],
-            mono: ['Consolas', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-        },
-        colors: {
-            border:{
-                light:'#E5E5E5',
-                dark:'#888888',
+            fontFamily: {
+                serif: ['宋体', 'SimSun', 'Songti SC', 'serif'],
+                mono: ['Consolas', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
             },
-            textContent:{
-                light: '#121212',
-                dark: '#D6D2CD',
+            colors: {
+                border: {
+                    light: '#E5E5E5',
+                    dark: '#888888',
+                },
+                textContent: {
+                    light: '#121212',
+                    dark: '#D6D2CD',
+                },
+                textPrimary: {
+                    light: '#000000',
+                    dark: '#FFFFFF',
+                },
+                textSecondary: {
+                    light: '#666666',
+                    dark: '#ABABAB',
+                },
+                bg0: {
+                    light: '#FFFFFF',
+                    dark: '#121212',
+                },
+                bg1: {
+                    light: '#FAFAFA',
+                    dark: '#272727',
+                }
             },
-            textPrimary:{
-                light: '#000000',
-                dark: '#FFFFFF',
-            },
-            textSecondary:{
-                light: '#666666',
-                dark: '#ABABAB',
-            },
-            bg0:{
-                light:'#FFFFFF',
-                dark:'#121212',
-            },
-            bg1:{
-                light:'#FAFAFA',
-                dark:'#272727',
-            }
-        },
         },
     },
     plugins: [],
