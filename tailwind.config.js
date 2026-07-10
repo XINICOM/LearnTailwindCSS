3c907707486ae9dd7278225576ba7d3d4ab05b1b
xinicom <XINICOM@outlook.com>
2026-07-10 15:57:09 +0800

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  darkMode: "media",
-  theme: {
-    extend: {
-      fontFamily: {
-        serif: ['宋体', 'SimSun', 'Songti SC', 'serif'],
-        mono: ['Consolas', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-      },
-      colors: {
-
-      },
-
+    content: ["./src/**/*.{html,js}"],
+    darkMode: "media",
+    theme: {
+        extend: {
+        fontFamily: {
+            serif: ['宋体', 'SimSun', 'Songti SC', 'serif'],
+            mono: ['Consolas', 'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+        },
+        colors: {
+            border:{
+                light:'#EBEBEB',
+                dark:'',
+            },
+            textPrimary:{
+                light: '#000000',
+                dark: '#FFFFFF',
+            },
+            bg0:{
+                light:'#FFFFFF',
+                dark:'#121212',
+            }
+        },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
